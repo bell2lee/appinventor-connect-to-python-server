9e0b77560870f1672dead9ca6b732af64f275ac4
Jonghwi Lee <paperlee@kookmin.ac.kr>
2017-06-15 05:36:37 +0900

    add remark_json_server.py

--- remark_json_server.py
+++ remark_json_server.py
@@ -3,20 +3,26 @@ from cgi import parse_qs
 import json
 
 def application(environ, start_response):
-    try:
+    # 전달 받은 값의 사이즈 구하기
+	try:
         request_body_size = int(environ.get('CONTENT_LENGTH', 0))
     except ValueError:
         request_body_size = 0
+	# 전달 받은 값을 넣기
     response_body = environ['wsgi.input'].read(request_body_size)
-    d = parse_qs(response_body)
-
+    # 분류해버리기
+	d = parse_qs(response_body)
+	
+	# 분류한거 가져와서 변수에 저장하기
     sentence = d.get('sentence', [''][0])[0]
     character = d.get('character', [''][0])[0]
 
+	# 과제 값 구하기
     length = len(sentence)
     re_count = sentence.count(character)
 
     status = '200 OK'
+	# json형식으로 변환
     response_body = json.dumps({'length':length, 'count':re_count})
 	
     response_headers = [
@@ -25,7 +31,8 @@ def application(environ, start_response):
     ]
     
     start_response(status, response_headers)
-
+	
+	# 저장한 값 리턴하기
     return [response_body]
 
 httpd = make_server('localhost', 8051, application)
